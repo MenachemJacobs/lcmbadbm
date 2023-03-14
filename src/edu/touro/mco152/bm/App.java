ed6878b7dbf13e8fae7ccd344450a3c7332cf04d
Elyah Weiser <eweiser@student.touro.edu>
2023-03-14 18:55:31 -0400

    Moved my package level javadoc comment to a new file called package-info.java. Because it was not showing up on the Javadoc.

--- src/edu/touro/mco152/bm/App.java
+++ src/edu/touro/mco152/bm/App.java
@@ -1,8 +1,3 @@
-/**
- * This package contains utility classes to run, process, and display benchmarks.
- * The benchmarks various requirements can be configured by the user.
- * It implements the Swing GUI and displays the information in a chart
- */
 package edu.touro.mco152.bm;
 
 import edu.touro.mco152.bm.persist.DiskRun;
---
+++ src/edu/touro/mco152/bm/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains utility classes to run, process, and display benchmarks.
+ * The benchmarks various requirements can be configured by the user.
+ * It implements the Swing GUI and displays the information in a chart
+ */
+package edu.touro.mco152.bm;
