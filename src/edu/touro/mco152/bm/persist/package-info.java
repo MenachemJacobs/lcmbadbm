b633e6310c8122877ad4ea460bfa4e53919acbcb
YackovLazar <ylazar2@student.touro.edu>
2023-03-15 10:06:17 -0400

    Edited the JavaDoc for package-info.java of edu.touro.mco152.bm.persist

--- src/edu/touro/mco152/bm/persist/package-info.java
+++ src/edu/touro/mco152/bm/persist/package-info.java
@@ -1,6 +1,9 @@
 /**
- * A container for utility classes that help with handling persistent
- * data. This data can be used to gather statistical information
- * about the machine being tested.
+ * Provides classes that get, store, delete, and manage data obtained through the benchmark in a database.
+ * <p>
+ * relies heavily on jakarta.&nbsp;persistence for the database
+ * <p>
+ * {@link edu.touro.mco152.bm.persist.DiskRun DiskRun} Provides an entity for the storage benchmark and methods for adding, removing, querying, and changing data on jakarta.&nbsp;persistence's database.
+ * {@link edu.touro.mco152.bm.persist.EM EM} Provides an EntityManager that to manage to help track and manage changes to the aforementioned database.
  */
 package edu.touro.mco152.bm.persist;
