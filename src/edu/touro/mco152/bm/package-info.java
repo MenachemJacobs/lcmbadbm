77744012a5c5ff068825f93faaa3902c0478b955
dovsoko <dovsokolin@gmail.com>
2023-03-14 23:38:48 -0400

    Added 1 package-info "edu.touro.mco152.bm"

---
+++ src/edu/touro/mco152/bm/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Runs a benchmarking program through a Graphic User Interface (GUI) using Swing.
+ * It saves information about each benchmark to keep a recording of all benchmarking.
+ *
+ * The App class is where everything will run and sets all the variables.
+ * The DiskMark class will track and record all the progress as the disk is reading or writing.
+ * The DiskWorker class runs the disk benchmarking by the help of extending to a Swing class called SwingWorker.
+ */
+package edu.touro.mco152.bm;
