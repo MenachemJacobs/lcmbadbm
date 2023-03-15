4c3de24c0332102642b47a6c2b11db09d2962d26
ChanochW <cwinogra@student.touro.edu>
2023-03-15 15:33:16 -0400

    Added a package-info JavaDoc file to the edu.touro.mco152.bm package to explain its purpose and operation.

---
+++ src/edu/touro/mco152/bm/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains classes that manage the UI and the disk benchmarking.
+ * It utilizes the {@link edu.touro.mco152.bm.ui} and {@link edu.touro.mco152.bm.persist} packages to operate.
+ * The {@link edu.touro.mco152.bm.App} class holds the main runnable of this program.
+ * <p>
+ * This package has dependencies on java swing within {@link edu.touro.mco152.bm.App} and {@link edu.touro.mco152.bm.DiskWorker}.
+ */
+package edu.touro.mco152.bm;
