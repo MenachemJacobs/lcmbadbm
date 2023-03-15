dbe52df931e4e66f54ade57106c882de0680b272
Avi Weinberg <aweinber18@student.touro.edu>
2023-03-15 12:26:39 -0400

    Updated persist package-info javadoc.

--- src/edu/touro/mco152/bm/persist/package-info.java
+++ src/edu/touro/mco152/bm/persist/package-info.java
@@ -1,6 +1,7 @@
 /**
- * A container for utility classes that help with handling persistent
- * data. This data can be used to gather statistical information
- * about the machine being tested.
+ * A container for utility classes that help with handling persistent data.
+ * <p>
+ * @see edu.touro.mco152.bm.persist.DiskRun the class
+ * which stores and serializes full benchmark results.
  */
 package edu.touro.mco152.bm.persist;
